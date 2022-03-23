49a324912ab238d88e6ec87991cb67bf5f899708
Gabriel <gabe.cs1997@gmail.com>
2022-03-23 10:43:01 -0400

    updated workspace

--- colorGame00.js
+++ colorGame00.js
@@ -1,16 +1,29 @@
-const Sqaure = ({id , player}) => {
+const Sqaure = ({id , newState}) => {
   const [color,setColor] = React.useState('green');
+  const [status, setStatus] = React.useState(null);
+  const xo = ["O", "X"];
+
   const palet = ['red', 'blue','green'];
   const getRandomColor = () => 
     palet[Math.floor(Math.random()*3)]
+
+    React.useEffect(() => {
+      console.log(`Render ${id}`);
+      return () => console.log(`unmounting Square ${id}`);
+    });
+
   
   // keep track of state of the Square 
   return (
     // change color of Square on click 
-    <button onClick={(e) => {
-     alert(`I am square ${id}`)
+    <button onClick= { e => {
+     let col = getRandomColor();
+     setColor(col);
+     let nextPlayer = newState({ id: id, color:col });
+     e.target.style.background = col;
+     setStatus(nextPlayer);
     }}> 
-      <h1>{player}</h1>
+      <h1>{xo[status]}</h1>
     </button>
   )
 }
@@ -20,9 +33,26 @@ const Sqaure = ({id , player}) => {
 
 const Board = () => {
   const [player, setPlayer] = React.useState(0);
+  const [state, setState] = React.useState([]);
+
+// set state here
   let status = `Player ${player}`;
+
+
+  // define newState function 
+
+const newState = (ob) => {
+  let nextplayer = (player + 1)  % 2;
+  setPlayer(nextplayer);
+  setState([...state, ob]);
+  console.log(`adding state ${JSON.stringify(state)}`);
+  status = `Player ${nextplayer}`;
+  return nextplayer;
+}
+
+
   function renderSquare(i) {
-    return<Sqaure id={i} player = {player}></Sqaure>;
+    return<Sqaure id={i} player = {player} newState={newState}></Sqaure>;
   }
   return (
     <div
